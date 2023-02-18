a9ac1102e6dfb3f286a969618a9ef549366f5d45
valeskamdev <valeskkitty@gmail.com>
2023-02-18 20:47:21 -0300

    project html structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+    <div>
+      <input type="text" placeholder="O que tenha para fazer..." />
+      <button>Adicionar</button>
+
+      <ul>
+        <li>Aprender os fundamentos da programação</li>
+        <li>Escolher uma linguagem de programação</li>
+        <li>Aprender a usar o Git e o GitHub</li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default App
