eb62e9087aeeec09091d225d8c83502b89491e1f
valeskamdev <valeskkitty@gmail.com>
2023-02-19 20:17:53 -0300

    changing color of completed tasks

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,38 @@ import { FaTrash, FaCheck } from "react-icons/fa";
 import { Container, ToDoList, Input, Button, ListItem} from './styles.js'
 
 function App() {
-  const [list, setList] = useState([{id: uuid(), task: "Olá"}]);
+  const [list, setList] = useState([{id: uuid(), task: "Olá", finished: true }]);
   const [inputTask, setInputTask] = useState('');
 
-  function inputMudou(event) {
+  function inputChanged(event) {
     setInputTask(event.target.value)
   }
 
-  function cliequeiBotao() {
-    setList([ ...list, { id: uuid(), task: inputTask }])
+  function buttonClicked() {
+    setList([ ...list, { id: uuid(), task: inputTask, finished: false }])
     console.log(list);
+  }
+
+  function endTask(id) {
+    const newList = list.map( item => (
+      item.id === id ? { ...item, finished: !item.finished } : item
+    ))
+    setList(newList)
   }
 
   return (
     
     <Container>
       <ToDoList>
-      <Input onChange={inputMudou} type="text" placeholder="O que tenho para fazer..." />
-      <Button onClick={cliequeiBotao}>Adicionar</Button>
+      <Input onChange={inputChanged} type="text" placeholder="O que tenho para fazer..." />
+      <Button onClick={buttonClicked}>Adicionar</Button>
 
       <ul>
         {
           list.map(item => (
-            <ListItem>
-              <FaCheck style={{ color: "#4F0FC5" }} />
-              <li key={item.id}>{item.task}</li>
+            <ListItem isFinished={item.finished} key={item.id}>
+              <FaCheck onClick={() => endTask(item.id)} style={{ color: "#4F0FC5" }} />
+              <li>{item.task}</li>
               <FaTrash style={{ color: "#FF0000" }} />
             </ListItem>
           ))
--- src/styles.js
+++ src/styles.js
@@ -41,8 +41,8 @@ export const Button = styled.button`
 `
 
 export const ListItem = styled.div`
+    background: ${ props => props.isFinished ? '#E8FF8B' : '#E4E4E4' };
     box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
-    background: #E4E4E4;
     border-radius: 5px;
     display: flex;
     align-items: center;
