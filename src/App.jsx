7e5cccd73a9fb6cceff2e4cb59cc0450ef4fbf47
valeskamdev <valeskkitty@gmail.com>
2023-02-18 22:10:20 -0300

    uuid

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { v4 as uuid } from 'uuid';
+
 function App() {
 
+  const list = [
+    { id: uuid(), task: "Aprender os fundamentos da programação"},
+    { id: uuid(), task: "Escolher uma linguagem de programação"},
+    { id: uuid(), task: "Aprender a usar o Git e o GitHub"}
+  ]
+
+  console.log(list);
+
+  function inputMudou(event) {
+    console.log(event);
+  }
+
+  function cliequeiBotao() {
+    console.log('cliquei no botao');
+  }
+
   return (
+    
     <div>
-      <input type="text" placeholder="O que tenha para fazer..." />
-      <button>Adicionar</button>
+      <input onChange={inputMudou} type="text" placeholder="O que tenha para fazer..." />
+      <button onClick={cliequeiBotao}>Adicionar</button>
 
       <ul>
-        <li>Aprender os fundamentos da programação</li>
-        <li>Escolher uma linguagem de programação</li>
-        <li>Aprender a usar o Git e o GitHub</li>
+        {
+          list.map(item => (
+            <li key={item.id}>{item.task}</li>
+          ))
+        }
       </ul>
       
     </div>
